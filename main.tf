d7804e122e584999b7b958d6daa4857f72722c2c
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2021-01-25 11:06:10 +0000

    refactor code

--- main.tf
+++ main.tf
@@ -1,223 +1,223 @@
 resource "aws_cloudwatch_metric_alarm" "dataplane_cpu-high" {
-  count = var.dataplane_cpu-enable ? 1 : 0
-  alarm_name = "dataplane_CPU_utilization-high" 
-  alarm_description = "Alarm for PAN high CPU utilization"
-  metric_name = "DataPlaneCPUUtilizationPct"
+  count               = var.dataplane_cpu-enable ? 1 : 0
+  alarm_name          = "dataplane_CPU_utilization-high"
+  alarm_description   = "Alarm for PAN high CPU utilization"
+  metric_name         = "DataPlaneCPUUtilizationPct"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.high_dataplanecpu_threshold 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.dataplane_cpu-scale_up_actions
-  period = var.period-dataplane_cpu
-  unit = "Percent"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.high_dataplanecpu_threshold
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.dataplane_cpu-scale_up_actions
+  period              = var.period-dataplane_cpu
+  unit                = "Percent"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "dataplane_cpu-low" {
-  count = var.dataplane_cpu-enable ? 1 : 0
-  alarm_name = "dataplane_CPU_utilization-low" 
-  alarm_description = "Alarm for PAN low CPU utilization"
-  metric_name = "DataPlaneCPUUtilizationPct"
+  count               = var.dataplane_cpu-enable ? 1 : 0
+  alarm_name          = "dataplane_CPU_utilization-low"
+  alarm_description   = "Alarm for PAN low CPU utilization"
+  metric_name         = "DataPlaneCPUUtilizationPct"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.low_dataplanecpu_threshold 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.dataplane_cpu-scale_down_actions
-  period = var.period-dataplane_cpu
-  unit = "Percent"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.low_dataplanecpu_threshold
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.dataplane_cpu-scale_down_actions
+  period              = var.period-dataplane_cpu
+  unit                = "Percent"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "active_sessions-high" {
-  count = var.active_sessions-enable ? 1 : 0
-  alarm_name = "active_sessions-high" 
-  alarm_description = "Alarm for high - Active Sessions"
-  metric_name = "panActiveSession"
+  count               = var.active_sessions-enable ? 1 : 0
+  alarm_name          = "active_sessions-high"
+  alarm_description   = "Alarm for high - Active Sessions"
+  metric_name         = "panActiveSession"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-active_sessions-high 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.active_sessions-scale_up_actions
-  period = var.period-active_sessions 
-  unit = "Count"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.threshold-active_sessions-high
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.active_sessions-scale_up_actions
+  period              = var.period-active_sessions
+  unit                = "Count"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "active_sessions-low" {
-  count = var.active_sessions-enable ? 1 : 0
-  alarm_name = "active_sessions-low" 
-  alarm_description = "Alarm for low - Active Sessions"
-  metric_name = "panActiveSession"
+  count               = var.active_sessions-enable ? 1 : 0
+  alarm_name          = "active_sessions-low"
+  alarm_description   = "Alarm for low - Active Sessions"
+  metric_name         = "panActiveSession"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-active_sessions-low 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.active_sessions-scale_down_actions
-  period = var.period-active_sessions
-  unit = "Count"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.threshold-active_sessions-low
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.active_sessions-scale_down_actions
+  period              = var.period-active_sessions
+  unit                = "Count"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "session_utilization-high" {
-  count = var.session_utilization-enable ? 1 : 0
-  alarm_name = "session_utilization-high" 
-  alarm_description = "Alarm for high - session utilization "
-  metric_name = "panSessionUtilization"
+  count               = var.session_utilization-enable ? 1 : 0
+  alarm_name          = "session_utilization-high"
+  alarm_description   = "Alarm for high - session utilization "
+  metric_name         = "panSessionUtilization"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-session_utilization-high 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.session_utilization-scale_up_actions
-  period = var.period-session_utilization 
-  unit = "Percent"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.threshold-session_utilization-high
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.session_utilization-scale_up_actions
+  period              = var.period-session_utilization
+  unit                = "Percent"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "session_utilization-low" {
-  count = var.session_utilization-enable ? 1 : 0
-  alarm_name = "session_utilization-low" 
-  alarm_description = "Alarm for low - Session Utilization "
-  metric_name = "panSessionUtilization"
+  count               = var.session_utilization-enable ? 1 : 0
+  alarm_name          = "session_utilization-low"
+  alarm_description   = "Alarm for low - Session Utilization "
+  metric_name         = "panSessionUtilization"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-session_utilization-low 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.session_utilization-scale_down_actions
-  period = var.period-session_utilization
-  unit = "Percent"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.threshold-session_utilization-low
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.session_utilization-scale_down_actions
+  period              = var.period-session_utilization
+  unit                = "Percent"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "GP_gateway_utilization-high" {
-  count = var.GP_gateway_utilization-enable ? 1 : 0
-  alarm_name = "GP_gateway_utilization-high" 
-  alarm_description = "Alarm for high - GP Gateway Utilization"
-  metric_name = "panGatewayUtilizationPct"
+  count               = var.GP_gateway_utilization-enable ? 1 : 0
+  alarm_name          = "GP_gateway_utilization-high"
+  alarm_description   = "Alarm for high - GP Gateway Utilization"
+  metric_name         = "panGatewayUtilizationPct"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-GP_Gateway_Utilization-high 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.GP_gateway_utilization-scale_up_actions
-  period = var.period-GP_gateway_utilization 
-  unit = "Percent"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.threshold-GP_Gateway_Utilization-high
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.GP_gateway_utilization-scale_up_actions
+  period              = var.period-GP_gateway_utilization
+  unit                = "Percent"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "GP_gateway_utilization-low" {
-  count = var.GP_gateway_utilization-enable ? 1 : 0
-  alarm_name = "GP_gateway_utilization-low" 
-  alarm_description = "Alarm for low - GP gateway utilization"
-  metric_name = "panGatewayUtilizationPct"
+  count               = var.GP_gateway_utilization-enable ? 1 : 0
+  alarm_name          = "GP_gateway_utilization-low"
+  alarm_description   = "Alarm for low - GP gateway utilization"
+  metric_name         = "panGatewayUtilizationPct"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-GP_gateway_utilization-low 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.GP_gateway_utilization-scale_down_actions
-  period = var.period-GP_gateway_utilization
-  unit = "Percent"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.threshold-GP_gateway_utilization-low
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.GP_gateway_utilization-scale_down_actions
+  period              = var.period-GP_gateway_utilization
+  unit                = "Percent"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "GP_active_tunnel-high" {
-  count = var.GP_active_tunnel-enable ? 1 : 0
-  alarm_name = "GP_active_tunnels-high" 
-  alarm_description = "Alarm for high - GP Active Tunnels"
-  metric_name = "panGPGWUtilizationActiveTunnels"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-GP_active_tunnel-high 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.GP_active_tunnel-scale_up_actions
-  period = var.period-GP_active_tunnel 
-  unit = "Count"
-  namespace = var.name_space 
+  count               = var.GP_active_tunnel-enable ? 1 : 0
+  alarm_name          = "GP_active_tunnels-high"
+  alarm_description   = "Alarm for high - GP Active Tunnels"
+  metric_name         = "panGPGWUtilizationActiveTunnels"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  threshold           = var.threshold-GP_active_tunnel-high
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.GP_active_tunnel-scale_up_actions
+  period              = var.period-GP_active_tunnel
+  unit                = "Count"
+  namespace           = var.namespace
 }
 
-resource "aws_cloudwatch_metric_alarm" "GP_active_tunnel-low" {
-  count = var.GP_active_tunnel-enable ? 1 : 0
-  alarm_name = "GP_active_tunnel-low" 
-  alarm_description = "Alarm for low - GP active tunnel"
-  metric_name = "panGPGWUtilizationActiveTunnels"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-GP_active_tunnel-low 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.GP_active_tunnel-scale_down_actions
-  period = var.period-GP_active_tunnel
-  unit = "Count"
-  namespace = var.name_space 
-}
+# resource "aws_cloudwatch_metric_alarm" "GP_active_tunnel-low" {
+#   count               = var.GP_active_tunnel-enable ? 1 : 0
+#   alarm_name          = "GP_active_tunnel-low"
+#   alarm_description   = "Alarm for low - GP active tunnel"
+#   metric_name         = "panGPGWUtilizationActiveTunnels"
+#   comparison_operator = "LessThanThreshold"
+#   evaluation_periods  = "1"
+#   threshold           = var.threshold-GP_active_tunnel-low
+#   statistic           = "Average"
+#   actions_enabled     = true
+#   alarm_actions       = var.GP_active_tunnel-scale_down_actions
+#   period              = var.period-GP_active_tunnel
+#   unit                = "Count"
+#   namespace           = var.namespace
+# }
 
 resource "aws_cloudwatch_metric_alarm" "dataplane_buffer_utilization-high" {
-  count = var.dataplane_buffer_utilization-enable ? 1 : 0
-  alarm_name = "dataplane_buffer_utilization-high" 
-  alarm_description = "Alarm for high - Dataplane Buffer Utilization"
-  metric_name = "DataPlanePacketBufferUtilization"
+  count               = var.dataplane_buffer_utilization-enable ? 1 : 0
+  alarm_name          = "dataplane_buffer_utilization-high"
+  alarm_description   = "Alarm for high - Dataplane Buffer Utilization"
+  metric_name         = "DataPlanePacketBufferUtilization"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-dataplane_buffer_utilization-high 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.dataplane_buffer_utilization-scale_up_actions
-  period = var.period-dataplane_buffer_utilization 
-  unit = "Percent"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.threshold-dataplane_buffer_utilization-high
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.dataplane_buffer_utilization-scale_up_actions
+  period              = var.period-dataplane_buffer_utilization
+  unit                = "Percent"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "dataplane_buffer_utilization-low" {
-  count = var.dataplane_buffer_utilization-enable ? 1 : 0
-  alarm_name = "dataplane_buffer_utilization-low" 
-  alarm_description = "Alarm for low - Dataplane Buffer utilization"
-  metric_name = "DataPlanePacketBufferUtilization"
+  count               = var.dataplane_buffer_utilization-enable ? 1 : 0
+  alarm_name          = "dataplane_buffer_utilization-low"
+  alarm_description   = "Alarm for low - Dataplane Buffer utilization"
+  metric_name         = "DataPlanePacketBufferUtilization"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-dataplane_buffer_utilization-low 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.dataplane_buffer_utilization-scale_down_actions
-  period = var.period-dataplane_buffer_utilization
-  unit = "Percent"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.threshold-dataplane_buffer_utilization-low
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.dataplane_buffer_utilization-scale_down_actions
+  period              = var.period-dataplane_buffer_utilization
+  unit                = "Percent"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "ssl_proxy_utilization-high" {
-  count = var.ssl_proxy_utilization-enable ? 1 : 0
-  alarm_name = "ssl_proxy_utilization-high" 
-  alarm_description = "Alarm for high - Session SSL Proxy Utilization"
-  metric_name = "panSessionSslProxyUtilization"
+  count               = var.ssl_proxy_utilization-enable ? 1 : 0
+  alarm_name          = "ssl_proxy_utilization-high"
+  alarm_description   = "Alarm for high - Session SSL Proxy Utilization"
+  metric_name         = "panSessionSslProxyUtilization"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-ssl_proxy_utilization-high 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.ssl_proxy_utilization-scale_up_actions
-  period = var.period-ssl_proxy_utilization 
-  unit = "Percent"
-  namespace = var.name_space 
+  evaluation_periods  = "1"
+  threshold           = var.threshold-ssl_proxy_utilization-high
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.ssl_proxy_utilization-scale_up_actions
+  period              = var.period-ssl_proxy_utilization
+  unit                = "Percent"
+  namespace           = var.namespace
 }
 
 resource "aws_cloudwatch_metric_alarm" "ssl_proxy_utilization-low" {
-  count = var.ssl_proxy_utilization-enable ? 1 : 0
-  alarm_name = "ssl_proxy_utilization-low" 
-  alarm_description = "Alarm for low - Session SSL Proxy Utilization"
-  metric_name = "DataPlanePacketBufferUtilization"
+  count               = var.ssl_proxy_utilization-enable ? 1 : 0
+  alarm_name          = "ssl_proxy_utilization-low"
+  alarm_description   = "Alarm for low - Session SSL Proxy Utilization"
+  metric_name         = "DataPlanePacketBufferUtilization"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods = "1"
-  threshold = var.threshold-ssl_proxy_utilization-low 
-  statistic = "Average"
-  actions_enabled = true
-  alarm_actions = var.ssl_proxy_utilization-scale_down_actions
-  period = var.period-ssl_proxy_utilization
-  unit = "Percent"
-  namespace = var.name_space 
- }
+  evaluation_periods  = "1"
+  threshold           = var.threshold-ssl_proxy_utilization-low
+  statistic           = "Average"
+  actions_enabled     = true
+  alarm_actions       = var.ssl_proxy_utilization-scale_down_actions
+  period              = var.period-ssl_proxy_utilization
+  unit                = "Percent"
+  namespace           = var.namespace
+}
--- variables.tf
+++ variables.tf
@@ -1,118 +1,118 @@
-variable "name_space" {
-  type = string
-  description = "name space"
-  default = "panfw"
+variable "namespace" {
+  type        = string
+  description = "namespace"
+  default     = "panfw"
 }
 #******************************************************************************
 # Metric Actions
 #******************************************************************************
 variable "dataplane_cpu-scale_up_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling up"
-  default = [] 
+  default     = []
 }
 variable "dataplane_cpu-scale_down_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling down"
-  default = []
+  default     = []
 }
 variable "active_sessions-scale_up_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling up"
-  default = [] 
+  default     = []
 }
 variable "active_sessions-scale_down_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling down"
-  default = []
+  default     = []
 }
 variable "session_utilization-scale_up_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling up"
-  default = [] 
+  default     = []
 }
 variable "session_utilization-scale_down_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling down"
-  default = []
+  default     = []
 }
 variable "GP_gateway_utilization-scale_up_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling up"
-  default = [] 
+  default     = []
 }
 variable "GP_gateway_utilization-scale_down_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling down"
-  default = []
+  default     = []
 }
 variable "GP_active_tunnel-scale_up_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling up"
-  default = [] 
+  default     = []
 }
 variable "GP_active_tunnel-scale_down_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling down"
-  default = []
+  default     = []
 }
 variable "dataplane_buffer_utilization-scale_up_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling up"
-  default = [] 
+  default     = []
 }
 variable "dataplane_buffer_utilization-scale_down_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling down"
-  default = []
+  default     = []
 }
 variable "ssl_proxy_utilization-scale_up_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling up"
-  default = [] 
+  default     = []
 }
 variable "ssl_proxy_utilization-scale_down_actions" {
-  type = list(string)
+  type        = list(string)
   description = "actions for scaling down"
-  default = []
+  default     = []
 }
 #******************************************************************************
 # Enable Metrics variables
 #******************************************************************************
 variable "dataplane_cpu-enable" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable Metric"
 }
 variable "active_sessions-enable" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable Metric"
 }
 variable "session_utilization-enable" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable Metric"
 }
 variable "GP_gateway_utilization-enable" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable Metric"
 }
 variable "GP_active_tunnel-enable" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable Metric"
 }
 variable "dataplane_buffer_utilization-enable" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable Metric"
 }
 
 variable "ssl_proxy_utilization-enable" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable Metric"
 }
 
@@ -120,111 +120,111 @@ variable "ssl_proxy_utilization-enable" {
 #  Threshold Variables
 #******************************************************************************
 variable "high_dataplanecpu_threshold" {
-  type = number
-  default = 999
-  description = "high_dataplanecpu_threshold" 
+  type        = number
+  default     = 999
+  description = "high_dataplanecpu_threshold"
 }
 variable "low_dataplanecpu_threshold" {
-  type = number
-  default = 999
-  description = "low_dataplanecpu_threshold" 
+  type        = number
+  default     = 999
+  description = "low_dataplanecpu_threshold"
 }
 variable "threshold-active_sessions-high" {
-  type = number
-  default = 100
-  description = "threshold-active_sessions-high" 
+  type        = number
+  default     = 100
+  description = "threshold-active_sessions-high"
 }
 variable "threshold-active_sessions-low" {
-  type = number
-  default = 100
+  type        = number
+  default     = 100
   description = "threshold-active_sessions-low"
-}  
+}
 variable "threshold-session_utilization-high" {
-  type = number
-  default = 999
-  description = "threshold-session_utilization-high" 
+  type        = number
+  default     = 999
+  description = "threshold-session_utilization-high"
 }
 variable "threshold-session_utilization-low" {
-  type = number
-  default = 999
-  description = "threshold-session_utilization-low" 
+  type        = number
+  default     = 999
+  description = "threshold-session_utilization-low"
 }
-variable "threshold-GP_Gateway_Utilization-high"{
-  type = number
-  default = 999
-  description = "threshold-GP_Gateway_Utilization-high" 
+variable "threshold-GP_Gateway_Utilization-high" {
+  type        = number
+  default     = 999
+  description = "threshold-GP_Gateway_Utilization-high"
 }
 variable "threshold-GP_gateway_utilization-low" {
-  type = number
-  default = 999
-  description = "threshold-GP_gateway_utilization-low" 
+  type        = number
+  default     = 999
+  description = "threshold-GP_gateway_utilization-low"
 }
-variable "threshold-GP_active_tunnel-high"{
-  type = number
-  default = 999
+variable "threshold-GP_active_tunnel-high" {
+  type        = number
+  default     = 1500
   description = "threshold-GP_active_tunnel-high"
 }
 variable "threshold-GP_active_tunnel-low" {
-  type = number
-  default = 999
-  description = "threshold-GP_active_tunnel-low" 
+  type        = number
+  default     = 750
+  description = "threshold-GP_active_tunnel-low"
 }
 variable "threshold-dataplane_buffer_utilization-high" {
-  type = number
-  default = 999
-  description = "threshold-dataplane_buffer_utilization-high" 
+  type        = number
+  default     = 999
+  description = "threshold-dataplane_buffer_utilization-high"
 }
 variable "threshold-dataplane_buffer_utilization-low" {
-  type = number
-  default = 999
-  description = "threshold-dataplane_buffer_utilization-low" 
+  type        = number
+  default     = 999
+  description = "threshold-dataplane_buffer_utilization-low"
 }
 variable "threshold-ssl_proxy_utilization-high" {
-  type = number
-  default = 999
-  description = "threshold-ssl_proxy_utilization-high" 
+  type        = number
+  default     = 999
+  description = "threshold-ssl_proxy_utilization-high"
 }
 variable "threshold-ssl_proxy_utilization-low" {
-  type = number
-  default = 999
-  description = "threshold-dataplane_buffer_utilization-low" 
+  type        = number
+  default     = 999
+  description = "threshold-dataplane_buffer_utilization-low"
 }
 #******************************************************************************
 #  Period Variables
 #******************************************************************************
 variable "period-dataplane_cpu" {
-  type = number
-  default = 300
+  type        = number
+  default     = 300
   description = "dataplanecpu_period"
 }
 variable "period-active_sessions" {
-  type = number
-  default = 300
+  type        = number
+  default     = 300
   description = "period-active_sessions"
 }
 variable "period-session_utilization" {
-  type = number
-  default = 300
+  type        = number
+  default     = 300
   description = "session_utilization"
 }
 variable "period-GP_gateway_utilization" {
-  type = number
-  default = 300
+  type        = number
+  default     = 300
   description = "GP_gateway_utilization"
- } 
+}
 variable "period-GP_active_tunnel" {
-  type = number
-  default = 300
+  type        = number
+  default     = 300
   description = "GP_active_tunnel"
- }
+}
 variable "period-dataplane_buffer_utilization" {
-  type = number
-  default = 300
+  type        = number
+  default     = 300
   description = "period-dataplane_buffer_utilization"
 }
 
 variable "period-ssl_proxy_utilization" {
-  type = number
-  default = 300
+  type        = number
+  default     = 300
   description = "period-ssl_proxy_utilization"
 }
